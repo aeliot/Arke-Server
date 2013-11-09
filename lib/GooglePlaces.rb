36a5e4dc75ce89ae8363718f989abcfa02432104
Adam Eliot <adameliot@gmail.com>
2013-11-09 17:58:01 -0500

    Trying to get Lat/Lng

--- lib/GooglePlaces.rb
+++ lib/GooglePlaces.rb
@@ -4,13 +4,15 @@ require "sinatra/base"
 
 
 class GooglePlaces
-  include HTTParty
   base_uri 'https://maps.googleapis.com/maps/api/place/textsearch/json'
   default_params :key => 'AIzaSyAAc16615kw98ZLpwRZhckJkhO-A55Xd-c', :sensor => 'true'
   format :json
 
   def self.getLocation(address)
-    result = JSON.parse(get('', :query => {:query => address.tr(" ", "+")}).body)
+    result = JSON.parse(HTTParty.get('', :query => {:query => address.tr(" ", "+")}).body)
+    
+    logger.debug(result)
+
     lat = result["results"].first["geometry"]["location"]["lat"]
     lng = result["results"].first["geometry"]["location"]["lng"]
     {:lat => lat, :lng => lng}
