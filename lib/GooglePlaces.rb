e360d42d91bb0c2b3ec0a073f607f222b5e3cae6
Adam Eliot <adameliot@gmail.com>
2013-11-09 02:00:49 -0500

    added google

---
+++ lib/GooglePlaces.rb
@@ -0,0 +1,20 @@
+require 'httparty'
+
+class GooglePlaces
+  include HTTParty
+  base_uri 'https://maps.googleapis.com/maps/api/place/textsearch/'
+  default_params :key => 'AIzaSyAAc16615kw98ZLpwRZhckJkhO-A55Xd-c', :sensor => 'true'
+  format :json
+
+  def self.getLocation(address)
+    result = get('json', :query => address)
+    result["results"].first["geometry"]["location"]
+  end
+
+end
+
+
+def genLocations(client)
+#  contact_class = client.materialize("Contact")
+#  emptyContacts = Contact.find_by_MailingLatitude(0)
+end
