dce059c6d8b3231c7e03db67076a83ccc598a367
Adam Eliot <adameliot@gmail.com>
2013-11-08 19:36:30 -0500

    blarg

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Arke::Application.routes.draw do
   resources :connects
 
-  root 'connects#auth'
+#  root 'connects#auth'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
