196f06121b56c097d114b793ea2e01bc36b64897
Adam Eliot <adameliot@gmail.com>
2013-11-09 21:23:49 -0500

    Trying to get Lat/Lng

--- lib/GooglePlaces.rb
+++ lib/GooglePlaces.rb
@@ -11,7 +11,24 @@ class GooglePlaces
     
     uri.query = URI.encode_www_form(params)
     raw = Net::HTTP.get(uri)
-    JSON.parse(raw)["results"].first["geometry"]["location"]
+    #JSON.parse(raw)["results"].first["geometry"]["location"]
+    parsedRaw = JSON.parse(raw)
+    if(parsedRaw.present?)
+      parsedResults = parsedRaw["results"]
+      if(parsedResults.present?)
+        geom = parsedResults.first["geometry"]
+        if(geom.present?)
+          loc = geom["location"]
+          return loc
+        else
+          print "Error: geometry nil"
+        end
+      else
+        print "Error: results nil"
+      end
+    else
+      print "Error: parsed raw nil"
+    end
   end
 end
 
